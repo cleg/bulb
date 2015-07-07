477e48be75f862d189996b5c680083cf14ff443d
Paul Dmitryev <clegger@gmail.com>
2015-07-07 14:03:57 +0200

    Added dummy API calls

--- src/app.js
+++ src/app.js
@@ -1,3 +1,4 @@
+var ajax = require('ajax');
 var UI = require('ui');
 var Vector2 = require('vector2');
 var Vibe = require('ui/vibe');
@@ -18,7 +19,7 @@ var val_field = new UI.Text({
   position: new Vector2(0, 65),
   size: new Vector2(114, 30),
   font: 'gothic-28-bold',
-  text: 'Value = ' + val,
+  text: '???',
   textAlign: 'center',
   color: 'black',
 });
@@ -50,7 +51,7 @@ function on_click(e) {
     }
   }
   val_field.text('Value = ' + val);    
-  console.log('Set value ' + val);
+  console.log('Value = ' + val);
 }
 
 function show_status(stat_text) {
@@ -64,8 +65,36 @@ function show_status(stat_text) {
 wind.on('click', 'up', on_click);
 wind.on('click', 'down', on_click);
 wind.on('click', 'select', function(e) {
-  show_status('GUT!');
-  console.log('http://ourapi.com/setval?value=' + val);
+  ajax({
+      'url': 'http://cleg.pagekite.me/api',
+      'method': 'post',
+      'type': 'json',
+      'data': {'num': val}
+    },
+    function(data, status, request) {
+      console.log('It\'s OK, server said: ' + JSON.stringify(data));
+      show_status('GUT !');
+    },
+    function(error, status, request) {
+      console.log('The ajax request failed: ' + error);
+      show_status('FAIL :(');
+    }
+  );
+  console.log('Tried to set value=' + val);
 });
+
+ajax(
+  {
+    url: 'http://cleg.pagekite.me/api',
+    type: 'json'
+  },
+  function(data, status, request) {
+    val = data.val;
+    val_field.text('Value = ' + val);
+  },
+  function(error, status, request) {
+    console.log('The ajax request failed: ' + error);
+  }
+);
 
 wind.show();
